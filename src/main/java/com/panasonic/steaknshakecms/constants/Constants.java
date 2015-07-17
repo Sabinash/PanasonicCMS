71a166307276160a3db616dfb29880ce8aaf24a9
abinash sahoo <sabhinash88@gmail.com>
2015-07-17 21:31:01 +0530

    Revert "Project Added"
    This reverts commit 857b798afa49e4a656625201d87e6dbda82a007f.

--- src/main/java/com/panasonic/steaknshakecms/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.panasonic.steaknshakecms.constants;
-
-public class Constants {
-
-	public static final String FAILED_TOUPLOAD = "You failed to upload because the file was empty.";
-	public static final String IMAGE_UPLOAD_PATH = "C:\\steaknshakecms\\image\\" ;
-}
--- src/main/java/com/panasonic/steaknshakecms/repository/MenuPageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.panasonic.steaknshakecms.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.panasonic.steaknshakecms.model.MenuPage;
-
-public interface MenuPageRepository extends MongoRepository<MenuPage, String> {
-
-}
